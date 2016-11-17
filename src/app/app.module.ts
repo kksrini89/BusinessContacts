7d91277a76b186455df698bb8416511a3bff6d49
Srinivasan K K <Srinivasan K K>
2016-11-17 08:34:32 +0530

    AngularFire2 setup is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAtlOI4flRCXIjUeI41a1mvPYK0mbfyTpk",
+  authDomain: "businesscontacts-3249b.firebaseapp.com",
+  databaseURL: "https://businesscontacts-3249b.firebaseio.com",
+  storageBucket: "businesscontacts-3249b.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
