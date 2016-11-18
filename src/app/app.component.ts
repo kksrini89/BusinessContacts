711da5e2d9d7c846af306737b3c3e1c6445ef8be
Srinivasan <kksrini89@gmail.com>
2016-11-18 23:41:11 +0530

    Frontend is designed with Foundation CSS Framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2';
 import { FirebaseService } from './service/firebase.service';
+
+import { Business } from './business';
+import { Category } from './category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   items: FirebaseListObservable<any[]>;
-  businesses: FirebaseListObservable<any[]>;
-  categories: FirebaseListObservable<any[]>;
+  appState: string;
+  activeKey: number;
+  businesses: Business[];
+  categories: Category[];
   constructor(private appService: FirebaseService) {
-    this.items = this.appService.getItems();//.subscribe(data => this.items = data);
-    this.businesses = this.appService.getBusiness();
-    this.categories = this.appService.getCategories();
+    this.items = this.appService.getItems();//.subscribe(data => this.items = data);   
+  }
+
+  ngOnInit() {
+    this.appService.getBusiness().subscribe(businesses => this.businesses = businesses);
+    this.appService.getCategories().subscribe(categories => this.categories = categories);
+  }
+
+  changeState(state, key) {
+    if (key) {
+      this.activeKey = key;
+    }
+    this.appState = state;
   }
 }
